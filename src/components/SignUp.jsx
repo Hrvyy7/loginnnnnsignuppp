444c6ad8de876ddf53452ea7f647a8eff9b868b1
Hrvyy7 <147082253+Hrvyy7@users.noreply.github.com>
2024-07-08 10:09:28 +0530

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,51 +1,40 @@
 // src/components/SignUp.jsx
 import React, { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
   const navigate = useNavigate();
 
   const handleSignUp = async (e) => {
     e.preventDefault();
     const auth = getAuth();
-
     try {
       await createUserWithEmailAndPassword(auth, email, password);
-      setSuccess('Sign up successful!');
-      setError('');
-      setEmail('');
-      setPassword('');
-      // Redirect to home or login page after successful sign-up
       navigate('/');
     } catch (error) {
-      setError(error.message);
-      setSuccess('');
+      alert('Sign Up failed: ' + error.message);
     }
   };
 
   return (
     <div className="form-container">
       <h2>Sign Up</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {success && <p style={{ color: 'green' }}>{success}</p>}
       <form onSubmit={handleSignUp}>
         <input
           type="email"
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
           required
         />
         <input
           type="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
           required
         />
         <button type="submit">Sign Up</button>
